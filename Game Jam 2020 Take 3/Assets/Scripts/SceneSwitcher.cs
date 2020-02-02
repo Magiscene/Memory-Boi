6c7a1a6f1d5e8a90bfb9effe021bc29e4ea342a6
alecrunge28 <55810579+alecrunge28@users.noreply.github.com>
2020-02-01 22:22:34 -0600

    Finished my levels

---
+++ Game Jam 2020 Take 3/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            SceneManager.LoadScene(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+           // SceneManager.LoadScene(1);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //This will get you the next scene
+        }
+    }
+}
