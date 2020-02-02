a39da84e4006de609750ca0f0f47adf196a47b2b
Magiscene <47087664+Magiscene@users.noreply.github.com>
2020-02-01 18:41:43 -0600

    Initial Commit

---
+++ Game Jam 2020 Take 3/Assets/Memories/Love.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Love : MonoBehaviour
+{
+    public bool loveFound = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Game Jam 2020 Take 3/Assets/Spawners/Platformer Spawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerSpawner : MonoBehaviour
+{
+    Accomplishment accom;
+    Fear f;
+    Love Lo;
+    Regret reg;
+    Peace pea;
+    Anger ang;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (accom.accomplishmentFound == true)
+        { 
+            
+        }
+
+        if (f.fearFound == true)
+        {
+
+        }
+
+        if (Lo.loveFound == true)
+        {
+
+        }
+
+        if (pea.peaceFound == true)
+        {
+
+        }
+
+        if (reg.regretFound == true)
+        {
+
+        }
+
+        if (ang.angerFound == true)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
